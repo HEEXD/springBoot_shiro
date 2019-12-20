1d72ce185d1318fe2cccb43f04da4cfbd11c6662
hee <heexudong@163.com>
2019-12-20 16:37:12 +0800

    super commit

--- src/main/java/com/hxd/ApplicationRun.java
+++ src/main/java/com/hxd/ApplicationRun.java
@@ -19,7 +19,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Realm：域，Shiro从从Realm获取安全数据（如用户、角色、权限），就是说SecurityManager要验证用户身份，
  *          那么它需要从Realm获取相应的用户进行比较以确定用户身份是否合法；也需要从Realm得到用户相应的角色/权限进行验证用户是否能进行操作；
  *          可以把Realm看成DataSource，即安全数据源。
- *
  */
 @SpringBootApplication
 @MapperScan("com.hxd.dao")
